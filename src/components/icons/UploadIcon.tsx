19b715e902ef9fd1e83d9c52d10d18745f92e2d1
劉錦 <liujin@littlegarden.com.cn>
2023-11-15 17:26:26 +0900

    upload button

--- src/components/icons/UploadIcon.tsx
+++ src/components/icons/UploadIcon.tsx
@@ -1,7 +1,7 @@
 import { JSX } from 'solid-js/jsx-runtime'
 const defaultButtonColor = '#3B81F6'
 export const UploadIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
-	<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-upload" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke={props.color ?? defaultButtonColor} fill="none" stroke-linecap="round" stroke-linejoin="round">
+	<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-upload" width="24" height="24" viewBox="-49.1 -49.1 589.15 589.15" stroke-width="2" stroke={props.color ?? defaultButtonColor} fill="none" stroke-linecap="round" stroke-linejoin="round">
 		<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
 		<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="3.9276400000000002"></g>
 		<g id="SVGRepo_iconCarrier">
